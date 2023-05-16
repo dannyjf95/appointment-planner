1f293fbf82392c500d5d54bf79b9b36b9470e5aa
Daniel Frith <46321457+dannyjf95@users.noreply.github.com>
2023-05-16 20:17:20 +0100

    add form and 3 inputs with relative info/props

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
+  currentName,
+  setCurrentName,
+  currentPhone,
+  setCurrentPhone,
+  currentEmail,
+  setCurrentEmail,
+  handleSubmit,
 }) => {
-
   //3 handlers for settingState
   const handleName = ({ target }) => {
     setCurrentName(target.value);
@@ -21,7 +20,37 @@ export const ContactForm = ({
     setCurrentEmail(target.value);
   };
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={currentName}
+          onChange={handleName}
+          placeholder="Contact Name"
+          required
+          aria-label="Contacts Full Name"
+        />
+        {/** */}
+        <input
+          type="email"
+          value={currentEmail}
+          onChange={handleEmail}
+          placeholder="Contact Email"
+          required
+          aria-label="Contacts Email Address"
+        />
+        {/** */}
+        <input
+          type="tel"
+          value={currentPhone}
+          onChange={handlePhone}
+          placeholder="Contact Phone Number (+44)"
+          required
+          pattern="/^(?:\+44|0)\d{10}$/"
+          aria-label="Contacts Phone Number"
+        />
+        <input type="Submit" aria-label="Add Contact" />
+      </form>
+    </>
   );
 };
-
