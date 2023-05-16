f5f859382a8d381f5a95dfdb12c52e799f8ffe15
Daniel Frith <46321457+dannyjf95@users.noreply.github.com>
2023-05-16 20:16:52 +0100

    add handlers to ContactForm.js for state setters

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -9,6 +9,17 @@ export const ContactForm = ({
   setEmail,
   handleSubmit
 }) => {
+
+  //3 handlers for settingState
+  const handleName = ({ target }) => {
+    setCurrentName(target.value);
+  };
+  const handlePhone = ({ target }) => {
+    setCurrentPhone(target.value);
+  };
+  const handleEmail = ({ target }) => {
+    setCurrentEmail(target.value);
+  };
   return (
     <></>
   );
