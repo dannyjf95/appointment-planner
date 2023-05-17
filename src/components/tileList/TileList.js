054140a22589521ea6ba93ce032f4bbe4e53dda1
Daniel Frith <46321457+dannyjf95@users.noreply.github.com>
2023-05-17 20:32:19 +0100

    console.log

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tile } from "../tile/Tile";
 export const TileList = ({ bothTiles }) => {
-  console.log(bothTiles);
+  // console.log(bothTiles);
 
   const mappedTiles = bothTiles.map((tile, index) => {
     return <Tile key={index} name={tile.name} {...tile} />;
